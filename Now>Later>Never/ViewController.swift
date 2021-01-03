cbe39524047afd4024ce2c7d1030a7a7f8c672d6
Denis Goloborodko <goldena_by@outlook.com>
2021-01-03 15:29:42 +0300

    Initial Commit

---
+++ Now>Later>Never/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Now>Later>Never
+//
+//  Created by Denis Goloborodko on 3.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Now>Later>NeverTests/Now_Later_NeverTests.swift
@@ -0,0 +1,33 @@
+//
+//  Now_Later_NeverTests.swift
+//  Now>Later>NeverTests
+//
+//  Created by Denis Goloborodko on 3.01.21.
+//
+
+import XCTest
+@testable import Now_Later_Never
+
+class Now_Later_NeverTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
