985643b4d6533d812a10a389ec02ca90ea54b820
Denis Goloborodko <goldena_by@outlook.com>
2021-01-18 00:09:37 +0300

    Version 0.31 DONE: 	Add Task Tab Added. TODO: 	Design Add Task Dialog 		Unit tests for Persistent Storage. BUGS: 	- PLAN: 	Add CloudKit as persistent storage.

---
+++ Now>Later>Never/Controllers & Utilites/AddTaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskViewController.swift
+//  Now>Later>Never
+//
+//  Created by Denis Goloborodko on 17.01.21.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
--- Now>Later>Never/Controllers & Utilites/TabBarController.swift
+++ Now>Later>Never/Controllers & Utilites/TabBarController.swift
@@ -7,20 +7,57 @@
 
 import UIKit
 
-class TabBarController: UITabBarController { //, UITabBarControllerDelegate {
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
     
     var persistentStorage = PersistentStorage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        delegate = self
+
+        delegate = self
+
+        // customizeAddTaskTabBarItem()
     }
+     
+    func customizeAddTaskTabBarItem() {
         
-//    func tabBarController(_ tabBarController: UITabBarController,
-//                          shouldSelect viewController: UIViewController) -> Bool {
-//
-//        return true
-//    }
+        var addTaskTabBarItem = UITabBarItem()
+        
+        let largeSFSymbol = UIImage.SymbolConfiguration(scale: .large)
+                
+        let addTaskNormalImage = UIImage(systemName: "plus.square", withConfiguration: largeSFSymbol)!
+            .withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+            .withTintColor(.systemGreen)
+        
+        let addTaskSelectedImage = UIImage(systemName: "plus.square.fill", withConfiguration: largeSFSymbol)!
+            .withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+            .withTintColor(.systemBlue)
+             
+        for item in self.tabBar.items! {
+            if item.tag == 1 {
+                addTaskTabBarItem = item
+            }
+        }
+                
+        addTaskTabBarItem.image = addTaskNormalImage
+        addTaskTabBarItem.selectedImage = addTaskSelectedImage
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController,
+                          shouldSelect viewController: UIViewController) -> Bool {
+        
+        guard type(of: viewController) == AddTaskViewController.self
+        else {
+            return true
+        }
+
+        guard let addTaskViewController = tabBarController.storyboard?.instantiateViewController(withIdentifier: "Add Task")
+        else {
+            fatalError("Could not instantiate ViewController for Add Task tab")
+        }
+        
+        tabBarController.present(addTaskViewController, animated: true)
+        return false
+    }
 
 }
