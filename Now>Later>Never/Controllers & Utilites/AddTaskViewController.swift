b8b157a91f11882aedd18254dffdc0a8849c7efd
Denis Goloborodko <goldena_by@outlook.com>
2021-01-20 00:57:14 +0300

    Version 0.33 DONE: 	The keyboard now won't block the fields TODO: 	Finish Design of Add Task Dialog 		Unit tests for Persistent Storage. BUGS: 	- PLAN: 	Add CloudKit as persistent storage.

--- Now>Later>Never/Controllers & Utilites/AddTaskViewController.swift
+++ Now>Later>Never/Controllers & Utilites/AddTaskViewController.swift
@@ -13,16 +13,55 @@ class AddTaskViewController: UIViewController {
     
     @IBOutlet weak var TaskTitleTextField: UITextField!
     
+    @IBOutlet weak var AddTaskBottomMarginConstraint: NSLayoutConstraint!
+
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         TaskTitleTextField.delegate = self
         
-        addTapOutsideTextFieldGestureRecognizer()
+        tapOutsideTextFieldGestureRecognizer()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
+    
+    @objc func keyboardWillShow(notification: Notification) {
 
-    func addTapOutsideTextFieldGestureRecognizer() {
+        guard let info = notification.userInfo,
+              let keyboardFrame = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        else {
+            return
+        }
+        
+        UIView.animate(withDuration: 0.1) {
+            self.AddTaskBottomMarginConstraint.constant = keyboardFrame.size.height
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+
+        guard let info = notification.userInfo,
+              let keyboardFrame = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        else {
+            return
+        }
+        
+        UIView.animate(withDuration: 0.1) {
+            self.AddTaskBottomMarginConstraint.constant = 0
+        }
+    }
+    
+    func tapOutsideTextFieldGestureRecognizer() {
         
         // Dismissed keyboard after tapping outside an edited field
         let tapOutsideTextField = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
@@ -38,7 +77,10 @@ class AddTaskViewController: UIViewController {
         
         let task = Task(title: TaskTitleTextField.text ?? "No title", category: .Work, date: Date())
         
+        // TODO: Replace with Persistend Storage Interface
         persistentStorage.createTask(task, on: .Today)
+        
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction private func CancelButtonPressed(_ sender: UIButton) {
