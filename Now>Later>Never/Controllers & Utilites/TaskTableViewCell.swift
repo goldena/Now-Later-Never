a1fd528985bae5d2a39abe122b97fc4c69d2392a
Denis Goloborodko <goldena_by@outlook.com>
2021-02-02 00:31:00 +0300

    Version 0.40.1 DONE: 	Switching to programmatic UI to make it more decoupled and reusable TODO: 	Finish Design of Add Task Dialog 		Unit tests for Persistent Storage. BUGS: 	- PLAN: 	Add CloudKit as persistent storage.

--- Now>Later>Never/Controllers & Utilites/TaskTableViewCell.swift
+++ Now>Later>Never/Controllers & Utilites/TaskTableViewCell.swift
@@ -12,23 +12,53 @@ class TaskTableViewCell: UITableViewCell {
     // MARK: - Properties
     var taskTitleLabel = UILabel()
     var taskDescriptionLabel = UILabel()
-    var taskDescriptionStackView = UIStackView()
+    var cellStackView = UIStackView()
     
     // MARK: - Methods - View Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        // Configure task title label
+        taskTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        taskTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        taskTitleLabel.numberOfLines = 1
+
+        // Configure task optional description
+        taskDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        taskDescriptionLabel.font = UIFont.systemFont(ofSize: 20)
+        taskDescriptionLabel.textColor = UIColor.systemGray
+        taskDescriptionLabel.numberOfLines = 0
+        
+        // Confrgure StackView
+        cellStackView.translatesAutoresizingMaskIntoConstraints = false
+        cellStackView.axis = .vertical
+        cellStackView.distribution = .fillEqually
+        
+        contentView.addSubview(cellStackView)
+        cellStackView.addArrangedSubview(taskTitleLabel)
+        cellStackView.addArrangedSubview(taskDescriptionLabel)
+        
+        // Add constraints to StackView
+        let margins = contentView.layoutMarginsGuide
+
+        NSLayoutConstraint.activate([
+            cellStackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            cellStackView.topAnchor.constraint(equalTo: margins.topAnchor),
+            cellStackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            cellStackView.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-            
-        backgroundColor = .gray
-        
-        taskTitleLabel.text = "test"
-        taskDescriptionLabel.text = "test"
-        contentView.addSubview(taskTitleLabel)
-        contentView.addSubview(taskDescriptionLabel)
-        contentView.addSubview(taskDescriptionStackView)
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
